e4b3a537a8b0663bebe83d2be54ab3ac809d3702
wenhao <wenhao.zhang@itwlw.com>
2016-11-03 15:03:32 +0800

    company

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -40,9 +40,9 @@ return array(
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'onethink_message', // 数据库名
+    'DB_NAME'   => 'zt_onethink', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'raspberry',  // 密码
+    'DB_PWD'    => '',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
